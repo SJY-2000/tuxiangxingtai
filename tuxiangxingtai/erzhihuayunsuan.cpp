c5f23a62ce68ea88d7787e6ef616fec9bf5d480b
SJY-2000 <2254789143@qq.com>
2020-10-28 17:46:55 +0800

    改

--- tuxiangxingtai/erzhihuayunsuan.cpp
+++ tuxiangxingtai/erzhihuayunsuan.cpp
@@ -1,21 +1,21 @@
-//#include<iostream>
-//#include<opencv2/opencv.hpp>
-//using namespace cv;
-//using namespace std;
-//int main()
-//{
-//	Mat srcMat = imread("G:/picture/1.jpg", 0);
-//	Mat result,result1,result2,result3,result4;
-//	Mat stt = getStructuringElement( MORPH_RECT, Size(3, 3), Point(-1, -1));
-//	threshold(srcMat, result, 100, 255, THRESH_BINARY);
-//	erode(result, result1, stt,Point(-1, -1), 1, BORDER_CONSTANT,morphologyDefaultBorderValue());
-//	dilate(result, result2, stt,Point(-1, -1), 1, BORDER_CONSTANT,morphologyDefaultBorderValue());
-//	morphologyEx(result, result3,2,stt,Point(-1, -1), 1, BORDER_CONSTANT,morphologyDefaultBorderValue());
-//	morphologyEx(result, result4, 3, stt,Point(-1, -1), 1, BORDER_CONSTANT,morphologyDefaultBorderValue());
-//	imshow("result", result);
-//	imshow("result1", result1);
-//	imshow("result2", result2);
-//	imshow("result3", result3);
-//	imshow("result4", result4);
-//	waitKey(0);
-//}
+#include<iostream>
+#include<opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+int main()
+{
+	Mat srcMat = imread("G:/picture/1.jpg", 0);
+	Mat result,result1,result2,result3,result4;
+	Mat stt = getStructuringElement( MORPH_RECT, Size(3, 3), Point(-1, -1));
+	threshold(srcMat, result, 100, 255, THRESH_BINARY);
+	erode(result, result1, stt,Point(-1, -1), 1, BORDER_CONSTANT,morphologyDefaultBorderValue());
+	dilate(result, result2, stt,Point(-1, -1), 1, BORDER_CONSTANT,morphologyDefaultBorderValue());
+	morphologyEx(result, result3,2,stt,Point(-1, -1), 1, BORDER_CONSTANT,morphologyDefaultBorderValue());
+	morphologyEx(result, result4, 3, stt,Point(-1, -1), 1, BORDER_CONSTANT,morphologyDefaultBorderValue());
+	imshow("result", result);
+	imshow("result1", result1);
+	imshow("result2", result2);
+	imshow("result3", result3);
+	imshow("result4", result4);
+	waitKey(0);
+}
--- tuxiangxingtai/yuandiangeshu.cpp
+++ tuxiangxingtai/yuandiangeshu.cpp
@@ -1,19 +1,19 @@
-//#include<iostream>
-//#include<opencv2/opencv.hpp>
-//using namespace cv;
-//using namespace std;
-//int main()
-//{
-//	Mat srcMat = imread("G:\\picture\\3.jpg",0);
-//	Mat result,result1;
-//	threshold(srcMat, result, 100, 255, THRESH_OTSU);	//大津法
-//	Mat stt = getStructuringElement(MORPH_RECT, Size(20,20), Point(-1, -1));
-//	morphologyEx(result, result1, 3, stt, Point(-1, -1), 1, BORDER_CONSTANT, morphologyDefaultBorderValue());	//函数默认白色为前景，所以应用闭运算
-//	imshow("result", result1);
-//	bitwise_not(result1, result1);	//前背景调换
-//	Mat labels, stats, centroids;
-//	int n = connectedComponentsWithStats(result1, labels, stats, centroids, 8, CV_32S);
-//	cout << "原点个数：" << n - 1 << endl;
-//	waitKey(0);
-//	return 0;
-//}
+#include<iostream>
+#include<opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+int main()
+{
+	Mat srcMat = imread("G:\\picture\\3.jpg",0);
+	Mat result,result1;
+	threshold(srcMat, result, 100, 255, THRESH_OTSU);	//大津法
+	Mat stt = getStructuringElement(MORPH_RECT, Size(20,20), Point(-1, -1));
+	morphologyEx(result, result1, 3, stt, Point(-1, -1), 1, BORDER_CONSTANT, morphologyDefaultBorderValue());	//函数默认白色为前景，所以应用闭运算
+	imshow("result", result1);
+	bitwise_not(result1, result1);	//前背景调换
+	Mat labels, stats, centroids;
+	int n = connectedComponentsWithStats(result1, labels, stats, centroids, 8, CV_32S);
+	cout << "原点个数：" << n - 1 << endl;
+	waitKey(0);
+	return 0;
+}
